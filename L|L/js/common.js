9adbcd7f1e8b727f07165de536a81c1c3fc33d22
leonidlist <l.list.pr@gmail.com>
2017-05-23 17:14:54 +0300

    Animations added

---
+++ L|L/js/common.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $(".circles").animated("fadeInUp", "fadeOutDown");
+  $(".bootstrap h4").animated("fadeInRightBig", "fadeOutRightBig");
+  $(".progressdesc").animated("fadeInUp", "fadeOutDown");
+});
