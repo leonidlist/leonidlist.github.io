c5ec9829a5112784877615f3afee962ee7a532a9
leonidlist <l.list.pr@gmail.com>
2017-09-09 16:00:23 +0300

    Design update. Added edit, delete button

--- VueJS/VueNotes/main.js
+++ VueJS/VueNotes/main.js
@@ -7,11 +7,13 @@ new Vue({
     notes: [
       {
         title: 'Выучить API Vue.js',
-        content: 'Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар взяв шрифтову гранку та склав на ній підбірку зразків шрифтів.'
+        content: 'Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар взяв шрифтову гранку та склав на ній підбірку зразків шрифтів.',
+        default: true
       },
       {
         title: 'Test title',
-        content: 'Some contentr here lorem ipus m dolor site amet'
+        content: 'Some contentr here lorem ipus m dolor site amet',
+        default: true
       },
     ]
   },
@@ -20,7 +22,8 @@ new Vue({
       if(this.newTitle != '' && this.newContent != ''){
         this.notes.push({
           title: this.newTitle,
-          content: this.newContent
+          content: this.newContent,
+          default: true
         })
         this.newTitle = ''
         this.newContent = ''
@@ -37,5 +40,13 @@ new Vue({
     deleteNote: function(te){
       this.notes.splice(this.notes.indexOf(te), 1)
     },
+    editNote: function(te){
+      index = this.notes.indexOf(te)
+      this.notes[index].default = false
+    },
+    editNoteConfirm: function(te){
+      index = this.notes.indexOf(te)
+      this.notes[index].default = true
+    }
   }
 })
