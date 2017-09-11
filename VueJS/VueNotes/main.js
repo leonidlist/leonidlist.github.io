14f76724b48e7ddf29128f8dfd8114305294690d
leonidlist <l.list.pr@gmail.com>
2017-09-11 16:37:25 +0300

    Notes app release 1.0

--- VueJS/VueNotes/main.js
+++ VueJS/VueNotes/main.js
@@ -1,52 +1,66 @@
+var config = {
+    apiKey: "AIzaSyDP9Eq7Acz5Tj29XzN96haotbLPFRPw0BA",
+    authDomain: "vuenotelist.firebaseapp.com",
+    databaseURL: "https://vuenotelist.firebaseio.com",
+    projectId: "vuenotelist",
+    storageBucket: "vuenotelist.appspot.com",
+    messagingSenderId: "772948068889"
+};
+firebase.initializeApp(config);
+
+var notesRef = firebase.database().ref('notes')
+
 new Vue({
   el: '#app',
   data: {
-    newTitle: 'Введите название заметки здесь.',
-    newContent: 'Создежимое заметки...',
     show: false,
-    notes: [
-      {
-        title: 'Выучить API Vue.js',
-        content: 'Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар взяв шрифтову гранку та склав на ній підбірку зразків шрифтів.',
-        default: true
-      },
-      {
-        title: 'Test title',
-        content: 'Some contentr here lorem ipus m dolor site amet',
-        default: true
-      },
-    ]
+    newNote: {
+      title: '',
+      content: '',
+      default: true,
+      date: 0
+    }
+  },
+  firebase: {
+    notes: notesRef
+  },
+  computed: {
+    dateOfNote(){
+      var d = +new Date
+      return d
+    }
   },
   methods: {
-    createNote: function(){
-      if(this.newTitle != '' && this.newContent != ''){
-        this.notes.push({
-          title: this.newTitle,
-          content: this.newContent,
-          default: true
-        })
-        this.newTitle = ''
-        this.newContent = ''
-      } else{
-        alert('Error')
-      }
-    },
     clearTitle: function(){
-      this.newTitle = ''
+      newNote.title = ''
     },
     clearContent: function(){
-      this.newContent = ''
+      newNote.content = ''
     },
     deleteNote: function(te){
-      this.notes.splice(this.notes.indexOf(te), 1)
+      notesRef.child(te['.key']).remove()
     },
     editNote: function(te){
-      index = this.notes.indexOf(te)
-      this.notes[index].default = false
+      notesRef.child(te['.key']).update({default: false})
     },
-    editNoteConfirm: function(te){
-      index = this.notes.indexOf(te)
-      this.notes[index].default = true
+    editNoteConfirm: function(te, tt, tc){
+      notesRef.child(te['.key']).update({
+        title: tt,
+        content: tc,
+        default: true
+      })
+    },
+    addNote: function(){
+      if(this.newNote.title !== '' && this.newNote.content !== ''){
+        var d = +new Date
+        this.newNote.date = d
+        notesRef.push(this.newNote);
+        this.newNote.title = ''
+        this.newNote.content = ''
+      }
+      else{
+        alert('Error')
+      }
     }
   }
-})
+});
