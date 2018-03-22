9cab2324fb1cce79bd24e73d77e74ba49c9570d8
Leonid List <list.leonid@gmail.com>
2018-03-23 00:02:43 +0200

    Design changes

--- demos/UNIQ/common.js
+++ demos/UNIQ/common.js
@@ -4,14 +4,20 @@ document.body.onload = function(){
         if(!preloader.classList.contains('done')){
             preloader.classList.add('done');
         }
-    }, 1300)
+    }, 1000)
 }
 
 // Анимация в блоке основого контента
 document.onreadystatechange = function(){
-    var someText = anime({
-        targets: 'h2',
-        scale: 0.9,
-        delay: 1800
-    });
+    // var someText = anime({
+    //     targets: 'h2',
+    //     scale: 0.9,
+    //     delay: 1800
+    // });
+
+    // var logo = anime({
+    //     targets: '.logo',
+    //     translateX: 300,
+    //     delay: 2000
+    // })
 };
