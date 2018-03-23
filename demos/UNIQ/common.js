af7f5006cb9dc2e3d8ff7ddbc23afbd6aca701f3
Leonid List <list.leonid@gmail.com>
2018-03-23 15:33:44 +0200

    Some changes

--- demos/UNIQ/common.js
+++ demos/UNIQ/common.js
@@ -3,21 +3,12 @@ document.body.onload = function(){
         var preloader = document.getElementById('page-preloader');
         if(!preloader.classList.contains('done')){
             preloader.classList.add('done');
+            
         }
-    }, 1000)
+        console.log('Done');
+    }, 1000);
+    setTimeout(function(){
+        document.getElementById('logo').classList.add('logo-anim');
+        console.log('Done class');
+    }, 1500);
 }
-
-// Анимация в блоке основого контента
-document.onreadystatechange = function(){
-    // var someText = anime({
-    //     targets: 'h2',
-    //     scale: 0.9,
-    //     delay: 1800
-    // });
-
-    // var logo = anime({
-    //     targets: '.logo',
-    //     translateX: 300,
-    //     delay: 2000
-    // })
-};
